398ba12eb40e905ceef6dad873f3c5fc61700668
NVadim <palaginvad@gmail.com>
2024-09-16 12:38:48 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  // Точка входа в приложение
+  entry: './src/index.js',
+
+  // Куда будут собираться файлы
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    clean: true, // Очистка предыдущей сборки
+  },
+
+  // Настройка devServer для разработки
+  devServer: {
+    static: path.resolve(__dirname, 'dist'),
+    port: 3000,
+    open: true, // Автоматически открывать браузер
+    hot: true,  // Включение горячей перезагрузки
+  },
+
+  // Загрузчики для обработки разных типов файлов
+  module: {
+    rules: [
+      // Для JavaScript/JSX файлов
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      // Для CSS файлов
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
+  // Плагины
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html', // Указываем путь к HTML шаблону
+      filename: 'index.html',
+    }),
+  ],
+
+  // Разрешаем использовать расширения .js и .jsx
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
